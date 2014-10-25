3bbe4e813feda07dbe5a4502541a5c11aeb8ae2d
Prateek Jain <prateekj@thoughtworks.com>
2014-10-25 15:11:54 +0530

    #03 : Prateek,Sumit : server is able to update the question on firebase.

--- app-test.js
+++ app-test.js
@@ -3,7 +3,29 @@ var assert = require('assert');
 var test = {};
 exports.test = test;
 
+
+var questionToSet = {
+	"Options" : [ null, "no", "yes", "i am not sure", "bilkul nahi" ],
+	"answer" : 2,
+	"question" : "is this my question?"
+};
+
 test.should_be_able_to_return_a_firebase_ref = function(){
 	lib.login();
 	assert.ok(lib.fireBaseRef instanceof lib.Firebase);
+}
+
+test.should_be_able_to_update_the_question = function(){
+	lib.Firebase = function(path){
+		this.path = path;
+		this.set = function(question){
+			assert.deepEqual(this.path, 'https://quizup.firebaseio.com/doNotChange/game/current_question');
+			assert.deepEqual(question, questionToSet);
+		};
+		this.authWithCustomToken = function(token, callBack){
+			assert.equal(token, "lLwXjNDm5algYXbUEEWekyVr30cgH9nQVW3yiDAw");
+		}
+	}
+    
+    lib.writeQuestion(questionToSet);
 }
--- app.js
+++ app.js
@@ -5,15 +5,22 @@ lib.Firebase = require("firebase");
 
 exports.lib = lib;
 
+var notFound = function(request, response){
+	response.writeHead(404, {'Content-Type': 'text/html'});
+	response.end("<b>Not Found</b>");
+}
+
 lib.login = function(){
 	lib.fireBaseRef = new lib.Firebase('https://quizup.firebaseio.com');
 }
 
-
-
-var notFound = function(request, response){
-	response.writeHead(404, {'Content-Type': 'text/html'});
-	response.end("<b>Not Found</b>");
+lib.writeQuestion = function(question){
+	var token = "lLwXjNDm5algYXbUEEWekyVr30cgH9nQVW3yiDAw";
+	var fireBaseWriteRef = new lib.Firebase("https://quizup.firebaseio.com/doNotChange/game/current_question");
+	fireBaseWriteRef.authWithCustomToken(token, function(error){
+		if(error) console.log("Login Failed");
+	})
+	fireBaseWriteRef.set(question);
 }
 
 lib.listener = function(request, response){
@@ -23,3 +30,5 @@ lib.listener = function(request, response){
 lib.login();
 console.log("Listening on 8086");
 http.createServer(lib.listener).listen(8086);
+
+// 'https://quizup.firebaseio.com/doNotChange/game/current_question'
